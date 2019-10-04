8538ea81918b368318c593af9a784c56a0ede640
ShivSharma2 <52864842+ShivSharma2@users.noreply.github.com>
2019-10-04 22:10:00 +0530

    Revert "Lms with Multi Level Leave Approval"

--- application/views/pages/pl/forbidden.php
+++
@@ -1,9 +0,0 @@
-<h1>Dostęp zabroniony</h1>
-
-<p>Nie masz pozwolenia na wykonanie tego działania.</p>
-
-<?php if($this->session->flashdata('msg')){ ?>
-<p>  
-  <?php echo $this->session->flashdata('msg'); ?>
-</p>
-<?php } ?>
