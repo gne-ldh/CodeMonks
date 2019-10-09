89a406472dfeb7d56e3627738d861f9fec623240
Abhishek Kumar <abhishek.srivastava16@ymail.com>
2019-10-09 21:13:06 +0530

    Post evaluation update

--- tnp_public/settings.py
+++ tnp_public/settings.py
@@ -85,15 +85,8 @@ WSGI_APPLICATION = 'tnp_public.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tnp',
-        'USER': 'mysqluser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
-        'OPTIONS': {
-                   'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
